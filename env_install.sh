f43fa7e30711991127ff9b5f9a40c0c2028f00d9
jian.zhou <jianzhou0420@outlook.com>
2025-06-07 18:02:23 +0930

    install_tmp

--- env_install.sh
+++ env_install.sh
@@ -1,8 +1,19 @@
-mamba env create -f conda_env,yaml
+mamba env create -f -y conda_env,yaml
 mamba activate equidiff
 
 pip uninstall robosuite
 cd third_party/robosuite
 pip install -e .
-cd ../..
 python -c "import robosuite; print(robosuite.__file__)"
+
+cd ../robosuite-task-zoo
+pip install -e .
+python -c "import robosuite_task_zoo; print(robosuite_task_zoo.__file__)"
+
+cd ../robomimic
+pip install -e .
+python -c "import robomimic; print(robomimic.__file__)"
+
+cd ../mimicgen
+pip install -e .
+python -c "import mimicgen; print(mimicgen.__file__)"
